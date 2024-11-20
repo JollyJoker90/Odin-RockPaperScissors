c2fdba45e28f0a934a4f29c92ff39204bfbb3ea5
JollyJoker90 <eike@LAPTOP-22I97KGM.>
2024-11-20 22:09:31 +0100

    added round logic

--- code.js
+++ code.js
@@ -6,10 +6,10 @@ capitalize = (str) => {
 }
 
 getComputerChoice = () => {
-    let computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice === 0) {
+    let computerSelection = Math.floor(Math.random() * 3);
+    if (computerSelection === 0) {
         return "Rock";
-    } else if (computerChoice === 1) {
+    } else if (computerSelection === 1) {
         return "Paper";
     } else {
         return "Scissors";
@@ -18,16 +18,30 @@ getComputerChoice = () => {
 
 getHumanChoice = () => {
     let humanSelection = capitalize(prompt("Make your decision:\nRock\nPaper\nScissors"));
-
+    if (!["Rock", "Paper", "Scissors"].includes(humanSelection)) {
+        alert("Wrong input..");
+        getHumanChoice()
+    }
     return humanSelection;
 }
 
 playRound = (human, computer) => {
-
+    if (human === computer) {
+        return
+    } else if ((human === "Rock" && computer === "Scissors") ||
+        (human === "Paper" && computer === "Rock") ||
+        (human === "Scissors" && computer === "Paper")) {
+        playerScore++;
+    } else if ((human === "Paper" && computer === "Scissors") ||
+        (human === "Scissors" && computer === "Rock") ||
+        (human === "Rock" && computer === "Paper")) {
+        computerScore++;
+    }
+    console.log(human, computer, playerScore, computerScore);
 }
 
 for (let i = 0; i < 10; i++) {
     console.log(getComputerChoice())
 }
-console.log("Human: " + getHumanChoice());
-console.log(capitalize("aTEST"))
+
+playRound(getHumanChoice(), getComputerChoice());
