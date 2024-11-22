6bfc90f863b3c2c261bc89af1627e9efb5d70710
JollyJoker90 <eike@LAPTOP-22I97KGM.>
2024-11-22 15:25:21 +0100

    added buttons testing js to interact with buttons

--- code.js
+++ code.js
@@ -38,25 +38,38 @@ playRound = (human, computer) => {
     }
 }
 
-playGame = () => {
-    let playerScore = 0;
-    let computerScore = 0;
+const gameWindow = document.querySelector(".gameWindow");
+const btnRock = document.querySelector(".rock");
+const btnPaper = document.querySelector(".paper");
+const btnScissors = document.querySelector(".scissors");
+const btns = document.querySelectorAll("button");
 
-    for (let i = 0; i < 5; i++) {
-        let roundWinner = playRound(getHumanChoice(), getComputerChoice());
-        if (roundWinner === "computer") {
-            computerScore++;
-        } else if (roundWinner === "player") {
-            playerScore++;
-        }
-    }
-    if (playerScore === computerScore) {
-        console.log(`It's a tie with ${playerScore} points each.`)
-    } else if (playerScore > computerScore) {
-        console.log(`Player wins ${playerScore} to ${computerScore}`)
-    } else {
-        console.log(`Computer wins ${computerScore} to ${playerScore}`)
-    }
-}
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        console.log("Test")
+    })
+})
 
-playGame();
+
+// playGame = () => {
+//     let playerScore = 0;
+//     let computerScore = 0;
+
+//     for (let i = 0; i < 5; i++) {
+//         let roundWinner = playRound(getHumanChoice(), getComputerChoice());
+//         if (roundWinner === "computer") {
+//             computerScore++;
+//         } else if (roundWinner === "player") {
+//             playerScore++;
+//         }
+//     }
+//     if (playerScore === computerScore) {
+//         console.log(`It's a tie with ${playerScore} points each.`)
+//     } else if (playerScore > computerScore) {
+//         console.log(`Player wins ${playerScore} to ${computerScore}`)
+//     } else {
+//         console.log(`Computer wins ${computerScore} to ${playerScore}`)
+//     }
+// }
+
+// playGame();
